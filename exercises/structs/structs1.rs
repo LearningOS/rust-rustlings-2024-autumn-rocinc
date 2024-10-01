b26a77849aa59bce1d0c773a90d4fb41dcca9baa
rocinc <roc63@outlook.com>
2024-10-01 09:09:23 +0800

    complete structs

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -3,18 +3,32 @@
 // Address all the TODOs to make the tests pass!
 //
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+// hint
 
 struct ColorClassicStruct {
+    red:i16,
+    blue:i16,
+    green:i16,
     // TODO: Something goes here
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(i16,i16,i16);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
+
+trait act
+{
+    fn action()->String;
+}
+
+impl act for UnitLikeStruct
+{
+    fn action()->String
+    {
+        String::from("UnitLikeStructs are fun!")
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -23,7 +37,11 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct{
+            red:0,
+            green:255,
+            blue:0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -32,8 +50,8 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct!
-        // let green =
+        // TODO: Inst,antiate a tuple struct!
+        let green = ColorTupleStruct(0,255,0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -43,9 +61,10 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
-        let message = format!("{:?}s are fun!", unit_like_struct);
+        //let unit_like_struct = UnitLikeStruct;
+        //let message = format!("{:?}s are fun!", unit_like_struct);
+        let message = UnitLikeStruct::action();
 
-        assert_eq!(message, "UnitLikeStructs are fun!");
+        assert_eq!(message, String::from("UnitLikeStructs are fun!"));
     }
 }
